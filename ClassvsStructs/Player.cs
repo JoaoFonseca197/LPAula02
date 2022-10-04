971cef2be4418e85fe62091bd6e7dd9e399faaba
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2022-10-04 10:18:52 +0100

    Ex PlayervsStruct

---
+++ ClassvsStructs/Player.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassvsStructs
+{
+    public struct Player
+    {
+        public int Health {get;set;}
+        public int Armor {get;set;}
+    }
+}
---
+++ ClassvsStructs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassvsStructs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player {Health = 100, Armor = 100};
+            Player player2 = player1;
+
+            player2.Health = 66;
+            player2.Armor = 11;
+
+            Console.WriteLine($"Player1 Health = {player1.Health} and Armor = {player1.Armor}");
+            Console.WriteLine($"Player1 Health = {player2.Health} and Armor = {player2.Armor}");
+            
+        }
+    }
+}
